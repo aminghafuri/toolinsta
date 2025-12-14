c536a09f50e74874bf205c4281e8ea4f6f65de14
Amin Ghafuri <aminghafuri021@gmail.com>
2025-12-14 07:24:58 -0700

    Try to fix offline mode

--- next.config.ts
+++ next.config.ts
@@ -170,7 +170,6 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 
 const nextConfig: NextConfig = {
   /* config options here */
-  turbopack: {},
 };
 
 export default withPWA(nextConfig);
